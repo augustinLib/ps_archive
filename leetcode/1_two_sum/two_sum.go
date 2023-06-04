6f562a47643573d12f1304da62eb2123be053b49
augustinLib <2bambitious@gmail.com>
2023-06-05 00:05:45 +0900

    Feat: Add solved problem

---
+++ leetcode/1_two_sum/two_sum.go
@@ -0,0 +1,11 @@
+// brute force
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
